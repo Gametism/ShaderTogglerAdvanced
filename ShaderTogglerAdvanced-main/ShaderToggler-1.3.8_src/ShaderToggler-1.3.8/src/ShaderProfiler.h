0afa02d68ccf85ce7180e1b5466581ec15bb588b
Gametism <157929762+Gametism@users.noreply.github.com>
2026-05-03 21:52:43 +0200

    Add files via upload

---
+++ ShaderTogglerAdvanced-main/ShaderToggler-1.3.8_src/ShaderToggler-1.3.8/src/ShaderProfiler.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <reshade.hpp>
+#include <unordered_map>
+
+struct ShaderStats {
+    uint32_t draw_calls = 0;
+};
+
+class ShaderProfiler {
+public:
+    void on_draw_call(reshade::api::pipeline pipeline);
+    void new_frame();
+    const std::unordered_map<reshade::api::pipeline, ShaderStats>& get_stats() const;
+
+private:
+    std::unordered_map<reshade::api::pipeline, ShaderStats> stats_;
+};
